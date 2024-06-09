869657c77133653fd5b45f2351eafa20a81114d0
diwakar-2112 <diwakarwork4586@gmail.com>
2024-06-10 04:10:06 +0530

    firstcommitTodo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { NgModule } from '@angular/core';
+import { NgModel } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+  currentDate =  new Date().toLocaleDateString();
+  newTask = '';
+  tasks:any[] = [];
+
+  addTask(){
+    if(this.newTask){
+      this.tasks.push(this.newTask);
+      this.newTask = '';
+    }
+  }
+  removeTask(task: string) {
+    this.tasks = this.tasks.filter(t => t !== task);
+  }
+  removeAllTask(){
+    this.tasks = [];
+  }
 }
